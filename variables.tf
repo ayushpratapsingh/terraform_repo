f4ef75f83a823c514fef05408984ec13d6a0bb90
ayush pratap singh <ayush.pratap906@gmail.com>
2024-03-03 16:59:31 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -4,8 +4,8 @@ variable "region" {
 
 
 variable "aws_access_key"{
-default = "AKIAXZ3J5D35N75YQZNN"
+default = ""
 }
 variable "aws_secret_key" {
-default = "mecn+yDIgUPaSdIXjr+zRdSbP21ln5yaI0FRou6i"
+default = ""
 }
